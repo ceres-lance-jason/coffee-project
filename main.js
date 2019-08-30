43f618c74258483deb0f7547533916d3665085a7
Jason Kempel <kempel.jason13@gmail.com>
2019-08-29 20:18:32 -0500

    coffee retry

--- main.js
+++ main.js
@@ -1,52 +1,39 @@
 "use strict";
 
 function renderCoffee(coffee) {
-    var html = '<div class="coffees">';
-    // html += '<div>' + coffee.id + '</div>';
+    var html = '<div id="coffee">';
+    // html += '<td>' + coffee.id + '</td>';
     html += '<h1>' + coffee.name + '</h1>';
     html += '<p>' + coffee.roast + '</p>';
-    html += '<div>';
+    html += '</div>';
 
     return html;
 }
 
 
-
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = 0; i < coffees.length; i++) {
+    for(var i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
-
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
+    var userQuery = userInput.value.toLowerCase();
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.name.toLowerCase().includes(userQuery) && coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
+        } else if (selectedRoast === "All") {
+            filteredCoffees = coffees;
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
-// function userCoffeeInput(e) {
-//     e.preventDefault();
-//     var coffeeName = userCoffee.value;
-//     var userCoffeeResults = [];
-//     coffees.forEach(function (coffee) {
-//         if (coffee.name.toLowerCase().includes(coffeeName.toLowerCase())) {
-//             userCoffeeResults.push(coffee);
-//         }
-//     });
-// }
-
-// Add functionality to search through the coffees by name, and display only the
-// coffees that match the provided search term (You will need to add an input
-// field to the existing form for this)
 
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -64,14 +51,21 @@ var coffees = [
     {id: 11, name: 'Espresso', roast: 'dark'},
     {id: 12, name: 'Viennese', roast: 'dark'},
     {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'}
+    {id: 14, name: 'French', roast: 'dark'},
 ];
+
+coffees.reverse();
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
+var userInput = document.querySelector('#input');
 
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+
+roastSelection.addEventListener('input', updateCoffees);
+
+userInput.addEventListener('input', updateCoffees);
 
